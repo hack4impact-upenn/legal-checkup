0e283b6f0ed50b5faadc40108c35d93e20e84764
Brandon <bobas@sas.upenn.edu>
2017-04-23 19:40:32 -0400

    updated with master and added routes

--- app/api/views.py
+++ app/api/views.py
@@ -7,3 +7,10 @@ from . import api
 def index():
     """View all apis."""
     return render_template('api/index.html')
+
+@api.route('/info/<int:api_id>', methods=['GET']) 
+# @login_required 
+def get_api_info(api_id): 
+	id = Api.query.get_or_404(api_id) 
+	return jsonify({'name': id.name, 'region': id.region, 'description' : id.description}) 
+
