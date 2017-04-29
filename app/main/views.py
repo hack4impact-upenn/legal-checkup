54b503767a54aa33d723f9987d4b8f6cbfd3a3b2
Stephanie Shi <stephanie.y.shi@gmail.com>
2017-04-29 17:21:46 -0400

    adding some frontend

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,10 @@ from . import main
 def index():
     return render_template('main/index.html')
 
+@main.route('/home')
+def home():
+    return render_template('main/home.html')
+
 @main.route('/about')
 def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
